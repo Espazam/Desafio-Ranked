39a7dc48dc76efd39f88874bb25e830eab3baa2c
Espazam <wesleymmenezes@hotmail.com>
2023-11-22 21:16:43 -0300

    terceiro commit

--- desafio.js
+++ desafio.js
@@ -1,1 +1,6 @@
-asdfasfdas
+testar("apocalipse" , "programa", "sucesso")
+function testar(teste1, teste2, teste3){
+    console.log("estamos efetuando o " + teste1)
+    console.log("para iniciarmos o " + teste2)
+    console.log("fim do mundo com " + teste3)
+}
