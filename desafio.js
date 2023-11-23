5141ceb95c8745e78930d52df474273a3f6396e7
Espazam <wesleymmenezes@hotmail.com>
2023-11-23 01:58:22 -0300

    desafio concluído

--- desafio.js
+++ desafio.js
@@ -1,6 +1,24 @@
-testar("apocalipse" , "programa", "sucesso")
-function testar(teste1, teste2, teste3){
-    console.log("estamos efetuando o " + teste1)
-    console.log("para iniciarmos o " + teste2)
-    console.log("fim do mundo com " + teste3)
+let vitoria = 100
+let derrota = 5
+let nivel;
+
+
+function saldovitoria(){
+    if (vitoria < 10) {
+        nivel = "Ferro";
+      } else if (vitoria >= 11 && vitoria <= 20) {
+        nivel = "Bronze";
+      } else if (vitoria >= 21 && vitoria <= 50) {
+          nivel = "Prata";
+      } else if (vitoria >= 51 && vitoria <= 80) {
+          nivel = "Ouro";
+      } else if (vitoria >= 81 && vitoria <= 90) {
+          nivel = "Diamante";
+      } else if (vitoria >= 91 && vitoria <= 100) {
+          nivel = "Lendário";
+      } else if (vitoria >= 101) {
+          nivel = "Imortal";
+      }
 }
+saldovitoria()
+console.log("O herói tem o saldo de " + (vitoria-derrota) + " está no nível de " + nivel)
